6c35cace87e49682e7b440053b9ca091a5a6970b
Vlad_Gorbatov <vlad.gorbatov2016@yandex.ru>
2024-08-30 10:42:03 +0300

    — class Animal replaced — pom.xml updated

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Animal {
+    public String name;
+    public String type;
+    public int age;
+    public double weight;
+    public String country;
+    public String color;
+    public String zooName;
+
+    public String toString() {
+        return "Animal [name=" + name + ", type=" + type + ", age=" + age + ", weight=" + weight + ", country="+country+ ", color="+color + ", zooName="+zooName+"]";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,16 +29,3 @@ public class Main {
     }
 }
 
-class Animal {
-    public String name;
-    public String type;
-    public int age;
-    public double weight;
-    public String country;
-    public String color;
-    public String zooName;
-
-    public String toString() {
-        return "Animal [name=" + name + ", type=" + type + ", age=" + age + ", weight=" + weight + ", country="+country+ ", color="+color + ", zooName="+zooName+"]";
-    }
-}
