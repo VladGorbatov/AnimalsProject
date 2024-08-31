1a3d172f4ab47a82788b1ee7d29c5e3bfc3a904a
Vlad_Gorbatov <vlad.gorbatov2016@yandex.ru>
2024-08-31 11:52:54 +0300

    some updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,7 +13,14 @@ public class Main {
         File listAnimals = new File("listAnimals.json");
         Animal animal = objectMapper.readValue(listAnimals, Animal.class);
 
-        System.out.println(animal);
+        System.out.println(animal+"\n");
+
+        Map<String, Object> map = objectMapper.readValue(listAnimals, new TypeReference<Map<String, Object>>() {
+        });
+
+        for(String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
     }
 }
 
